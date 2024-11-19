8e65cf25acd39034679829a569efba1c509788e7
abroa01 <142853163+abroa01@users.noreply.github.com>
2024-11-19 17:35:21 -0500

    Update puppeteer-test.js

--- puppeteer-test.js
+++ puppeteer-test.js
@@ -21,7 +21,7 @@ const launchOptions = {  };
     const page = await browser.newPage();
     console.log('New page opened.');
 
-    const timeout = 8000;
+    const timeout = 10000;
     page.setDefaultTimeout(timeout);
 
     {
